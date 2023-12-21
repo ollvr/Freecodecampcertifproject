e17faad505e1580d7c5e3f32d10325dc96af1257
ollvr <omarobouhawelo15@gmail.com>
2023-12-21 17:37:18 +0100

    change v4

--- index.js
+++ index.js
@@ -26,35 +26,30 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-/*
-You should provide your own project, not the example URL.
-Passed:A request to /api/:date? with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date in milliseconds (as type Number)
-Failed:A request to /api/:date? with a valid date should return a JSON object with a utc key that is a string of the input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
-Failed:A request to /api/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
-Failed:Your project can handle dates that can be successfully parsed by new Date(date_string)
-Failed:If the input date string is invalid, the API returns an object having the structure { error : "Invalid Date" }
-Passed:An empty date parameter should return the current time in a JSON object with a unix key
-Failed:An empty date parameter should return the current time in a JSON object with a utc key
 
-
-
-*/
 
 app.get("/api/:date_string?", function (req, res) {
   let date =   req.params.date_string
-  if(!date){
-    const currentTime = new Date();
-    res.json({ unix: currentTime.getTime(), utc: currentTime.toUTCString() });
-  }
-  else{
-    let dateObj = new Date(date);
-    if(dateObj.toString() === "Invalid Date"){
+  const getUtcFormat = (date) => {
+    return date.toUTCString();
+  };
+
+  if (!date) {
+    let now = new Date();
+    res.json({ unix: now.getTime(), utc: getUtcFormat(now) });
+  } else {
+    
+    let parsedDate = !isNaN(date) 
+      ? new Date(parseInt(date)) 
+      : new Date(date);
+  
+    if (!isNaN(parsedDate)) {
+      res.json({ unix: parsedDate.getTime(), utc: getUtcFormat(parsedDate) });
+    } else {
       res.json({ error: "Invalid Date" });
     }
-    else{
-      res.json({ unix: dateObj.getTime(), utc: dateObj.toUTCString() });
-    }
   }
+
 
 })
  
