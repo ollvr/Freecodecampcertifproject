b21497c55a850c1127090741a393a1519cc24379
ollvr <omarobouhawelo15@gmail.com>
2023-12-21 17:06:26 +0100

    changes

--- index.js
+++ index.js
@@ -26,16 +26,37 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+/*
+You should provide your own project, not the example URL.
+Passed:A request to /api/:date? with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date in milliseconds (as type Number)
+Failed:A request to /api/:date? with a valid date should return a JSON object with a utc key that is a string of the input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
+Failed:A request to /api/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
+Failed:Your project can handle dates that can be successfully parsed by new Date(date_string)
+Failed:If the input date string is invalid, the API returns an object having the structure { error : "Invalid Date" }
+Passed:An empty date parameter should return the current time in a JSON object with a unix key
+Failed:An empty date parameter should return the current time in a JSON object with a utc key
+
+
+
+*/
 
 app.get("/api/:date_string?", function (req, res) {
   let date = req.params.date_string
-  if (date == undefined) {
-    date = Date.now()
-  } else {
-    date = Date.parse(date)
+  if(date === undefined){
+    res.json({unix: Date.now(), utc: new Date().toUTCString()})
   }
-  res.json({unix: date, natural: new Date(date)})
+  else if(date.match(/\d{4}-\d{2}-\d{2}/)){
+    res.json({unix: new Date(date).getTime(), utc: new Date(date).toUTCString()})
+  }
+  else if (date.match(/\d{5,}/)){
+    res.json({unix: parseInt(date), utc: new Date(parseInt(date)).toUTCString()})
+  }
+  else{
+    res.json({error: "Invalid Date"})
+  }
 })
+ 
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
